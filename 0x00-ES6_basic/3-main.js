c856372af4d673f3cc40235f45a6f2034fe7c114
victorcreativee <tuyizerevictor@gmail.com>
2025-04-16 00:13:34 +0200

    3

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,9 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
+
+
+// console.log("Script is running");
+
