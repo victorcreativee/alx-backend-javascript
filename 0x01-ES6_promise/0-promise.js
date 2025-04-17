24d51bca03b76daa72a7c64102e0c93f776c504b
victorcreativee <tuyizerevictor@gmail.com>
2025-04-17 15:24:32 +0200

    0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-export default function getResponseFromAPI () {
-    return new Promise((resolve, reject) => {
-        resolve("Success");
+export default function getResponseFromAPI() {
+    return new Promise((resolve) => {
+        resolve('Success');
     });
 }
