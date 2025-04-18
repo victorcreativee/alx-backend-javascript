59b10ba71c9e306b62b8f76795478e661230c135
victorcreativee <tuyizerevictor@gmail.com>
2025-04-18 03:12:19 +0200

    Rest

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,10 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve) => resolve);
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
   }
+  
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, upload])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    }));
+}
