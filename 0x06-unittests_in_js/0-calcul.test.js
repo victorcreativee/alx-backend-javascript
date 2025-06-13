fa1ce918bfd81cc5e6ab91e8659481319a6e533b
victorcreativee <tuyizerevictor@gmail.com>
2025-06-13 16:26:56 +0200

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('should round and sum correctly', () => {
+    it('should return 4 when given 1 and 3', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.4, 4.5), 6);
+    });
+
+    it('should return 5 when given 1 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 when given 1.2 and 3.7', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 when given 1.5 and 3.7', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
